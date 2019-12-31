0cd7622c789c659847083b1dbb03a5987ec98af9
Rakshan Thapar <rakshanthapar32@gmail.com>
2019-12-31 21:50:44 +0530

    happy New Year Firework Integration

--- index.js
+++ index.js
@@ -3,18 +3,30 @@ function test(){
     image[0].style.display="none";
 }
 
-function rightFlip(){
+function setRotation(){
     roationDeg=Math.floor(Math.random() * 10);
     var root=document.documentElement;
     if(roationDeg<=2){
         roationDeg=5;
     }
-    root.style.setProperty("--rotationDeg",roationDeg*180+"deg");
+    if(roationDeg%2!=0){
+        roationDeg++;
+    }
+    
+    return roationDeg;
+}
+function rightFlip(){
+    var rotate=setRotation();
+    var root=document.documentElement;
+    root.style.setProperty("--rotationDeg",rotate*180+"deg");
     var image=document.getElementById("profile");
     image.classList.add("rightFlip");
     image.addEventListener("animationend",function(){
         image.src="./2020.jpg";
+        document.getElementById("instructions").innerHTML="Happy New Year!!"
         image.classList.remove("rightFlip");
+        document.getElementById("before").classList.add("before");
+        document.getElementById("after").classList.add("after");
     });
     
 }
