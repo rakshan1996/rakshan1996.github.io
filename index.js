7d9129c787174a8a2ce6f93756e8fe42d5ada566
Rakshan Thapar <rakshanthapar32@gmail.com>
2019-12-31 16:39:18 +0530

    New year Check In

---
+++ index.js
@@ -0,0 +1,21 @@
+function test(){
+    var image=document.getElementsByClassName("profile");
+    image[0].style.display="none";
+}
+
+function rightFlip(){
+    console.log("fuck");
+    roationDeg=Math.floor(Math.random() * 10);
+    var root=document.documentElement;
+    if(roationDeg<=2){
+        roationDeg=5;
+    }
+    root.style.setProperty("--rotationDeg",roationDeg*180+"deg");
+    var image=document.getElementById("profile");
+    image.classList.add("rightFlip");
+    image.addEventListener("animationend",function(){
+        image.src="./2020.jpg";
+        image.classList.remove("rightFlip");
+    });
+    
+}
